5132eac7c79a1afc188af3af5f3b25cff2c7bf15
Carlos Peix <carlos.peix@gmail.com>
2023-08-28 20:20:16 -0300

    Discount for specific product category

--- Model1/Discount.cs
+++ Model1/Discount.cs
@@ -2,18 +2,33 @@
 
 public class Discount
 {
-    public Discount(string name, decimal percentage = 0m)
+    public Discount(string name, decimal percentage = 0m, Category? category = default)
     {
         Name = name;
         Percentage = percentage;
+        Category = category;
     }
 
     public int Id { get; private set; }
     public string Name { get; set; }
-    public decimal Percentage { get; set; }
+    public decimal Percentage { get; private set; }
+    public Category? Category { get; private set; }
 
     internal bool AppliesTo(Order order)
     {
-        return true;
+        if (Category == null)
+        {
+            return true;
+        }
+
+        foreach (var orderItem in order.Items)
+        {
+            if (orderItem.Product.Category.Equals(Category))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Tests/DiscountTests.cs
+++ Tests/DiscountTests.cs
@@ -5,24 +5,28 @@ namespace Tests;
 public class DiscountTests
 {
     Customer aCustomer;
-    Country aCountry;
-    Category aCategory;
-    Product aProduct;
+    Country argentina;
+    Category programmingCategory;
+    Category sportsCategory;
+    Product programmingProduct;
+    Product sportsProduct;
 
     [SetUp]
     public void Setup()
     {
-        aCountry = new Country("Argentina");
-        aCategory = new Category("Programming");
-        aCustomer = new Customer("Sabados Tech", aCountry);
-        aProduct = new Product("Vim", aCategory, 9.95m);
+        argentina = new Country("Argentina");
+        programmingCategory = new Category("Programming");
+        sportsCategory = new Category("Electronics");
+        aCustomer = new Customer("Sabados Tech", argentina);
+        programmingProduct = new Product("Vim", programmingCategory, 9.95m);
+        sportsProduct = new Product("Googles", sportsCategory, 299.95m);
     }
 
     [Test]
     public void NoDiscountDoesNotChangeTotal()
     {
         var order = new Order(aCustomer);
-        order.AddItem(aProduct, 1);
+        order.AddItem(programmingProduct, 1);
         Assert.That(order.Total, Is.EqualTo(9.95m));
 
         var discount = new Discount("No discount");
@@ -35,12 +39,38 @@ public class DiscountTests
     public void CouponTotalDiscountChangeTotal()
     {
         var order = new Order(aCustomer);
-        order.AddItem(aProduct, 1);
+        order.AddItem(programmingProduct, 1);
         Assert.That(order.Total, Is.EqualTo(9.95m));
 
         var discount = new Discount(name: "Coupon", percentage: 10);
         order.ApplyDiscount(discount);
 
         Assert.That(order.Total, Is.EqualTo(9.95m * 0.9m));
+    }
+
+    [Test]
+    public void SportsCouponDoesNotChangeTotalOnElectronicsOrder()
+    {
+        var order = new Order(aCustomer);
+        order.AddItem(programmingProduct, 1);
+        Assert.That(order.Total, Is.EqualTo(9.95m));
+
+        var discount = new Discount(name: "Sports coupon", percentage: 8, category: sportsCategory);
+        order.ApplyDiscount(discount);
+
+        Assert.That(order.Total, Is.EqualTo(9.95m));
+    }
+
+    [Test]
+    public void SportsCouponChangeTotalOnSportsOrder()
+    {
+        var order = new Order(aCustomer);
+        order.AddItem(sportsProduct, 1);
+        Assert.That(order.Total, Is.EqualTo(299.95m));
+
+        var discount = new Discount(name: "Sports coupon", percentage: 8, category: sportsCategory);
+        order.ApplyDiscount(discount);
+
+        Assert.That(order.Total, Is.EqualTo(299.95m * 0.92m));
     }
 }
